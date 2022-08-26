11dcad99f82ade73721443f954800b63cb406354
Romain Pelisse <belaran@gmail.com>
2022-08-26 16:37:54 +0200

    Refactor jws jobs to match reponame change

--- jobs/ansible.groovy
+++ jobs/ansible.groovy
@@ -3,7 +3,7 @@ def downstreamCIJob(projectName, moleculeBuildId, projectPrefix = "ansible-downs
 }
 // Jobs to release productized version of Ansible Collection
 new ansibleCollection.Builder(collectionName:'redhat-csp-download').build(this)
-new ansibleCollection.Builder(collectionName:'jws-ansible-playbook').build(this)
+new ansibleCollection.Builder(collectionName:'jws').build(this)
 new ansibleCollection.Builder(collectionName:'ansible_collections_jcliff').build(this)
 new ansibleCollection.Builder(collectionName:'wildfly', downstreamName: 'jboss_eap').build(this)
 new ansibleCollection.Builder(collectionName:'infinispan', downstreamName: 'jboss_data_grid').build(this)
@@ -14,7 +14,7 @@ EapView.jobList(this, 'Ansible Collections', 'ansible-collection.*')
 //   Note that each CI job needs to increment the moleculeBuildId as
 //   this translate into a port number for SSHd running on the slave
 //   container (and thus, needs to be unique).
-new ansibleCi.Builder(projectName:'jws-ansible-playbook', moleculeBuildId: 22001).build(this)
+new ansibleCi.Builder(projectName:'jws', moleculeBuildId: 22001).build(this)
 new ansibleCi.Builder(projectName:'wildfly', moleculeBuildId: 23001).build(this)
 new ansibleCi.Builder(projectName:'infinispan', moleculeBuildId: 25001).build(this)
 new ansibleCi.Builder(projectName:'keycloak', moleculeBuildId: 26001).build(this)
@@ -23,7 +23,7 @@ new ansibleCi.Builder(projectName:'jws-dot', moleculeBuildId: 28001, gitUrl: "gi
 //new ansibleCi.Builder(projectName:'zeus', moleculeBuildId: 29001, gitUrl: "https://github.com/jboss-set/", branch: 'olympus').build(this)
 EapView.jobList(this, 'Ansible CI', 'ansible-ci.*')
 // CI jobs for downstream (Janus generated) collections
-downstreamCIJob('jws-ansible-playbook', "50001")
+downstreamCIJob('jws', "50001")
 downstreamCIJob('jboss_eap', "50002")
 downstreamCIJob('jws-dot', "50003")
 EapView.jobList(this, 'Ansible Downstream CI', 'ansible-downstream-ci.*$')
@@ -35,7 +35,7 @@ new ansibleCi.Builder(projectName:'flange-demo', branch: 'master', projectPrefix
 EapView.jobList(this, 'Ansible Demos', '^.*-demo')
 // Janus jobs - generating downstream collections
 new ansible.Builder(projectName:'janus', jobSuffix: '-redhat_csp_download', playbook: 'playbooks/redhat_csp_download.yml').build(this)
-new ansible.Builder(projectName:'janus', jobSuffix: '-jws-ansible-playbook', playbook: 'playbooks/jws.yml').build(this)
+new ansible.Builder(projectName:'janus', jobSuffix: '-jws', playbook: 'playbooks/jws.yml').build(this)
 new ansible.Builder(projectName:'janus', jobSuffix: '-jboss_eap', playbook: 'playbooks/jboss_eap.yml').build(this)
 new ansible.Builder(projectName:'janus', jobSuffix: '-jboss_data_grid', playbook: 'playbooks/jboss_data_grid.yml').build(this)
 new ansible.Builder(projectName:'janus', jobSuffix: '-rh_sso', playbook: 'playbooks/rh_sso.yml').build(this)
