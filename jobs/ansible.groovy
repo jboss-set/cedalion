79a0181ec7095fac3e04fe0f936eb6998c6be322
Guido Grazioli <ggraziol@redhat.com>
2022-09-05 15:20:20 +0200

    update amq builder job

--- jobs/ansible.groovy
+++ jobs/ansible.groovy
@@ -1,26 +1,25 @@
-def upstreamCIJob(projectName, moleculeBuildId, scenarioName = "--all")
+def upstreamCIJob(projectName, moleculeBuildId, scenarioName = "--all") {
   new ansibleCi.Builder(projectName: projectName, moleculeBuildId: moleculeBuildId, scenarioName: scenarioName, downloadServerUrl: '').build(this)
 }
-def downstreamCIJob(projectName, moleculeBuildId, projectPrefix = "ansible-downstream-ci", pipelineFile = "pipelines/ansible-downstream-ci-pipeline", pathToScript = "molecule-downstream.sh", scenarioName = "--all") {
-  new ansibleCi.Builder(projectName: projectName, projectPrefix: projectPrefix, pipelineFile: pipelineFile, pathToScript: pathToScript, moleculeBuildId: moleculeBuildId, scenarioName: scenarioName).build(this)
+def downstreamCIJob(projectName, moleculeBuildId, scenarioName = "--all") {
+  new ansibleCi.Builder(projectName: projectName, moleculeBuildId: moleculeBuildId, scenarioName: scenarioName, projectPrefix: "ansible-downstream-ci", pipelineFile: "pipelines/ansible-downstream-ci-pipeline", pathToScript: "molecule-downstream.sh").build(this)
 }
 // CI Jobs for Ansible Middleware
 //   Note that each CI job needs to increment the moleculeBuildId as
 //   this translate into a port number for SSHd running on the slave
 //   container (and thus, needs to be unique).
 upstreamCIJob('jws', 22001)
-upstreamCIJob('wildfly', moleculeBuildId: 23001)
-upstreamCIJob('infinispan', moleculeBuildId: 25001)
-upstreamCIJob('keycloak', moleculeBuildId: 26001)
-upstreamCIJob('amq', moleculeBuildId: 27001, scenarioName: "default,amq_upgrade")
+upstreamCIJob('wildfly', 23001)
+upstreamCIJob('infinispan', 25001)
+upstreamCIJob('keycloak', 26001)
+upstreamCIJob('amq', 27001, "default,amq_upgrade")
 //new ansibleCi.Builder(projectName:'zeus', moleculeBuildId: 29001, gitUrl: "https://github.com/jboss-set/", branch: 'olympus').build(this)
 EapView.jobList(this, 'Ansible CI', 'ansible-ci.*')
 // CI jobs for downstream (Janus generated) collections
-downstreamCIJob('jws', "30001")
-downstreamCIJob('jboss_eap', "30002")
-downstreamCIJob('jws-dot', "30003")
-//TOFIX: downstreamCIJob('amq', "30004", scenarioName: "default,amq_upgrade")
-new ansibleCi.Builder(projectName:'amq_broker', scenarioName: "default,amq_upgrade", projectUpstreamName: 'amq', projectPrefix: "ansible-downstream-ci", pipelineFile: "pipelines/ansible-downstream-ci-pipeline", pathToScript: "molecule-downstream.sh", moleculeBuildId: 30004).build(this)
+downstreamCIJob('jws', 30001)
+downstreamCIJob('jboss_eap', 30002)
+downstreamCIJob('jws-dot', 30003)
+downstreamCIJob('amq', 30004, "default,amq_upgrade")
 EapView.jobList(this, 'Ansible Downstream CI', 'ansible-downstream-ci.*$')
 // CI Jobs for demos
 new ansibleCi.Builder(projectName:'wildfly-cluster-demo', projectPrefix: 'ansible', moleculeBuildId: 40001).build(this)
