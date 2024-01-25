4c109c518797f5b13ab48e811eb7c36a2bb2589a
Romain Pelisse <belaran@gmail.com>
2024-01-25 17:38:05 +0100

    ansible: fix amq default play

--- jobs/ansible.groovy
+++ jobs/ansible.groovy
@@ -181,6 +181,6 @@ downstreamRunnerJob('jws','playbook', 'redhat_csp_download', '/webserver/5.6.0/j
 downstreamRunnerJob('eap', 'playbook', 'redhat_csp_download', '/eap7/7.4.0/jboss-eap-7.4.0.zip')
 downstreamRunnerJob('sso', 'keycloak', 'runtimes_common', '/sso/7.6.0/rh-sso-7.6.0-server-dist.zip')
 downstreamRunnerJob('amq_broker', 'activemq', 'runtimes_common', '/amq/broker/7.9.4/amq-broker-7.9.4-bin.zip')
-downstreamRunnerJob('amq_streams', 'playbooks/playbook.yml', 'runtimes_common', '/amq/streams/2.5.1/')
+downstreamRunnerJob('amq_streams', '', 'runtimes_common', '/amq/streams/2.5.1/')
 downstreamRunnerJob('rhbk', 'keycloak_quarkus', 'runtimes_common', '/keycloak/22.0.6/rhbk-22.0.6.zip')
 EapView.jobList(this, 'Ansible Downstream Runner', '^ansible-downstream-runner-.*$')
