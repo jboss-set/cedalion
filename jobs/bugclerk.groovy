a5caa07da0472fb2a8ab9e7a42c1f8f341ea2900
Romain Pelisse <belaran@gmail.com>
2024-03-20 13:32:39 +0100

    SET-730: Decomission of BugClerk

--- jobs/bugclerk.groovy
+++
@@ -1,6 +0,0 @@
-new bugclerk.Builder(
-            jobName: 'bugclerk-reports-jira-eap740-unresolved',
-            branch: 'EAP_740'
-        ).build(this)
-
-EapView.jobList(this, 'bugclerk', 'bugclerk.*')
